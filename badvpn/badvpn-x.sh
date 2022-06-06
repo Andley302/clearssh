cf109b1d78e0a67d3da01ac85745186115b78cba
Andley S <39422775+Andley302@users.noreply.github.com>
2022-06-06 20:21:59 -0300

    Update badvpn-x.sh

--- badvpn/badvpn-x.sh
+++ badvpn/badvpn-x.sh
@@ -6,9 +6,9 @@ rm -rf badvpn-udpgw;
 echo "INSTALADOR BADVPN X BY @sisudragon";
 sleep 5;
 clear;
-wget https://raw.githubusercontent.com/Andley302/utils/main/badvpn/antcrashudp.sh -O /bin/antcrashudp.sh > /dev/null 2>&1
+wget https://raw.githubusercontent.com/Andley302/clearssh/main/badvpn/badvpnx-files/antcrashudp.sh -O /bin/antcrashudp.sh > /dev/null 2>&1
 chmod +x /bin/antcrashudp.sh;
-wget https://raw.githubusercontent.com/Andley302/utils/main/badvpn/badvpn-udpgw -O /bin/badvpn-udpgw > /dev/null 2>&1
+wget https://raw.githubusercontent.com/Andley302/clearssh/main/badvpn/badvpnx-files/badvpn-udpgw -O /bin/badvpn-udpgw > /dev/null 2>&1
 chmod +x /bin/badvpn-udpgw;
 clear;
 echo "INSTALANDO...";
