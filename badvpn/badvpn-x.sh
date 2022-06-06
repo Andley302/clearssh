08c05e55d3f1071538477b45243f0a6e436e0f77
Andley S <39422775+Andley302@users.noreply.github.com>
2022-06-06 20:19:20 -0300

    Create badvpn-x.sh

---
+++ badvpn/badvpn-x.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear;
+cd /bin;
+rm -rf antcrashudp.sh;
+rm -rf badvpn-udpgw;
+echo "INSTALADOR BADVPN X BY @sisudragon";
+sleep 5;
+clear;
+wget https://raw.githubusercontent.com/Andley302/utils/main/badvpn/antcrashudp.sh -O /bin/antcrashudp.sh > /dev/null 2>&1
+chmod +x /bin/antcrashudp.sh;
+wget https://raw.githubusercontent.com/Andley302/utils/main/badvpn/badvpn-udpgw -O /bin/badvpn-udpgw > /dev/null 2>&1
+chmod +x /bin/badvpn-udpgw;
+clear;
+echo "INSTALANDO...";
+sleep 5;
+echo -e "ps x | grep 'udpvpn' | grep -v 'grep' || screen -dmS udpvpn antcrashudp.sh" >>/etc/autostart;
+ps x | grep 'udpvpn' | grep -v 'grep' || screen -dmS udpvpn antcrashudp.sh;
+clear;
+echo "BADVPN X INSTALADO COM SUCESSO!";
+
