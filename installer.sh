e5b76505b13ccf855eb92293084b71dc228586b3
Andley S <39422775+Andley302@users.noreply.github.com>
2023-02-02 13:18:21 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,6 +3,9 @@
 sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+sudo systemctl stop systemd-resolved;
+sudo systemctl disable systemd-resolved;
+sudo systemctl mask systemd-resolved;
 clear;
 echo "ClearSSH - Iniciando instalação...";
 sleep 5;
